479a511b4e80d36c8e2412a412e1f88e65bc2c66
EdConnell <edwardconnell@gmail.com>
2013-10-03 09:58:51 -0500

    Added database config to app

--- config/environment.rb
+++ config/environment.rb
@@ -23,10 +23,13 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
+
+require APP_ROOT.join('config', 'database')
 
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
