be36074bf8ea97067d49bb5454590b5d43729c44
EdConnell <edwardconnell@gmail.com>
2014-02-04 16:30:37 -0600

    Added feature to allow people to play any cohort.  Fixed typo in controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,48 +1,66 @@
 get '/' do
+  @cohorts = Cohort.all
+  if authenticated?
+    @cohorts.delete_if {|cohort| cohort.name == users_cohort}
+  end
   erb :index
 end
 
 get '/play' do
-  if current_user
-    @cohort = current_user.cohort
-    @game = Game.create(cohort_id: @cohort.id, user_id: current_user.id)
+  if authenticated?
+    @cohort = params[:cohort]
+    @game = Game.create(cohort_id: @cohort, user_id: current_user.id)
     @game.set_number_of_cards!
     redirect "/play/#{@game.id}"
   else
-    @error = "Error:  You must first log in before you can play."
+    @error = "Error:  Please log in first."
     erb :index
   end
 end
 
 get '/play/:game_id' do
-  @game = Game.find(params[:game_id])
-  if @game.user_id != current_user.id || @game.is_finished?
-    @error = "Error: This is not your game or game has already been finished."
-    erb :index
+  if authenticated?
+    @game = Game.find(params[:game_id])
+    if @game.user_id != current_user.id || @game.is_finished?
+      @error = "Error: This is not your game or the game has already been finished."
+      erb :index
+    else
+      @card = @game.next_card
+      @names = (@game.get_names << @card.name).shuffle
+      erb :game
+    end
   else
-    @card = @game.next_card
-    @names = (@game.get_names << @card.name).shuffle
-    erb :game
+    @error = "Error:  Please log in first."
+    erb :index
   end
 end
 
 post '/play/:game_id' do
-  @game = Game.find(params[:game_id])
-  @guess = params[:guess]
-  if @game.user_id != current_user.id
-    @error = "Error: This is not your game or has already been finished."
-    erb :index
-  else
-    @game.card_check(@guess)
-    if @game.is_finished?
-      redirect "/gameover"
+  if authenticated?
+    @game = Game.find(params[:game_id])
+    @guess = params[:guess]
+    if @game.user_id != current_user.id
+      @error = "Error: This is not your game or the game has already been finished."
+      erb :index
     else
-      redirect "/play/#{@game.id}"
+      @game.card_check(@guess)
+      if @game.is_finished?
+        redirect "/gameover"
+      else
+        redirect "/play/#{@game.id}"
+      end
     end
+  else
+    @error = "Error:  Please log in first."
+    erb :index
   end
 end
 
 get '/gameover' do
-  @game = current_user.last_game
-  erb :index
+  if authenticated?
+    @game = current_user.last_game
+    erb :index
+  else
+    @error = "Error:  Please log in first."
+  end
 end
--- app/helpers/users.rb
+++ app/helpers/users.rb
@@ -7,6 +7,10 @@ helpers do
     @current_user ||= User.find(session[:user_id])
   end
 
+  def users_cohort
+    current_user.cohort.name
+  end
+
   def gravatar_icon
     "#{current_user.gravatar_url}?size=40"
   end
